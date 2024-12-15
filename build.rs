b2283d21471a86eb09320d74f10956b52fe201ce
Clément DOUIN <clement.douin@posteo.net>
2024-12-15 17:27:36 +0100

    fix build.rs on apple

--- build.rs
+++ build.rs
@@ -1,7 +1,11 @@
-use std::fs;
-
 fn main() {
-    let _ = fs::remove_file("./src/secret_service/dbus_blocking/api.rs");
+    #[cfg(feature = "ss-dbus-std")]
+    generate_dbus_apis();
+}
+
+#[cfg(feature = "ss-dbus-std")]
+fn generate_dbus_apis() {
+    let _ = std::fs::remove_file("./src/secret_service/dbus_blocking/api.rs");
 
     let xml = include_str!("./org.freedesktop.Secrets.xml");
 
@@ -9,11 +13,11 @@ fn main() {
     opts.methodtype = None;
 
     #[cfg(feature = "ss-dbus-std")]
-    generate_blocking_api(xml, &mut opts);
+    generate_dbus_blocking_api(xml, &mut opts);
 }
 
 #[cfg(feature = "ss-dbus-std")]
-fn generate_blocking_api(xml: &str, opts: &mut dbus_codegen::GenOpts) {
+fn generate_dbus_blocking_api(xml: &str, opts: &mut dbus_codegen::GenOpts) {
     opts.connectiontype = dbus_codegen::ConnectionType::Blocking;
 
     let api = dbus_codegen::generate(xml, &opts).expect("should generate D-Bus blocking API");
