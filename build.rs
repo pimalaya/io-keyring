7ec1575c4b0ec9e2894eb85ec935a19098d3ca6b
Clément DOUIN <clement.douin@posteo.net>
2024-12-15 17:33:57 +0100

    move ss xml file

--- build.rs
+++ build.rs
@@ -7,7 +7,7 @@ fn main() {
 fn generate_dbus_apis() {
     let _ = std::fs::remove_file("./src/secret_service/dbus_blocking/api.rs");
 
-    let xml = include_str!("./org.freedesktop.Secrets.xml");
+    let xml = include_str!("./src/secret_service/api.xml");
 
     let mut opts = dbus_codegen::GenOpts::default();
     opts.methodtype = None;
@@ -22,6 +22,6 @@ fn generate_dbus_blocking_api(xml: &str, opts: &mut dbus_codegen::GenOpts) {
 
     let api = dbus_codegen::generate(xml, &opts).expect("should generate D-Bus blocking API");
 
-    fs::write("./src/secret_service/dbus_blocking/api.rs", api)
+    std::fs::write("./src/secret_service/dbus_blocking/api.rs", api)
         .expect("should write generated Secret Service blocking API");
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,14 @@
 #![cfg_attr(docsrs, feature(doc_cfg, doc_auto_cfg))]
 #![doc = include_str!("../README.md")]
 
+mod io;
+
 #[cfg(target_vendor = "apple")]
 #[cfg(feature = "apple-native-std")]
 pub mod apple;
-pub mod event;
-mod io;
 #[cfg(target_os = "linux")]
 #[cfg(feature = "ss-dbus-std")]
 pub mod secret_service;
-pub mod state;
 #[cfg(target_os = "windows")]
 #[cfg(feature = "windows-native")]
 pub mod windows;
